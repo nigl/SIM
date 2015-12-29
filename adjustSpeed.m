0c8914381f8ce476f78aab392d7b6af8aebe5e0e
blacky2 <schwarz-philipp@t-online.de>
2015-12-29 19:33:02 +0100

    removed unused function

--- adjustSpeed.m
+++ adjustSpeed.m
@@ -1,8 +1,8 @@
 function [Speed_new] = adjustSpeed(Speed_now, Car_now, Obstacles, SpeedMax)
 
-    %Beschleunigen für Autos die nicht vor Hindernissen stehen
+    %Beschleunigen fï¿½r Autos die nicht vor Hindernissen stehen
     Speed = min(SpeedMax, Speed_now+1);
-    FreeAhead = freeCellsM(Car_now, Obstacles, SpeedMax);
+    FreeAhead = freeCells(Car_now, Obstacles, SpeedMax);
     
     %bechleunige die zu beschleunigenden und die anderen gleich lassen
     %TempCells= ((FreeAhead < Speed_now).*Speed_now + (FreeAhead >= Speed_now).*Speed).*(Car_now ~= 0);
