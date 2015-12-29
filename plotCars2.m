84a00fc21f5fcbbe365592cc2d36a35efa4041ee
nigl <Martin-Kraus@gmx.de>
2015-12-29 22:54:55 +0100

    Achsen angepasst, starten jetzt bei null und gehen bis 2*CellsNumber+1

--- plotCars2.m
+++ plotCars2.m
@@ -7,8 +7,8 @@ for j=1:size(CellsH,2)
     Y1 = CellsV(1:CellsNumber, j, 2);
     Y2 = CellsV(CellsNumber+1:CellsNumber*2+1, j, 2);
     
-    position13 = (-CellsNumber:-1);
-    position24 = (0:CellsNumber);
+    position13 = (0:CellsNumber);
+    position24 = (CellsNumber+1:2*CellsNumber+1);
 
     CarsLeft = position13(X1 ~= 0);
     CarsRight = position24(X2 ~= 0);
@@ -16,17 +16,17 @@ for j=1:size(CellsH,2)
     CarsUp = position24(Y2 ~= 0);
   
     
-    XV = [zeros(size(CarsDown)), zeros(size(CarsUp))];
+    XV = CellsNumber*[ones(size(CarsDown)), ones(size(CarsUp))];
     YV = [CarsDown, CarsUp];
     
     XH = [CarsLeft, CarsRight];
-    YH = [zeros(size(CarsLeft)),  zeros(size(CarsRight))];
+    YH = CellsNumber*[ones(size(CarsLeft)),  ones(size(CarsRight))];
     plot(fig, XV, YV, 'o', XH, YH, 'or',  ...
-                    [-CellsNumber-1, CellsNumber+1], [-1, -1], '-b',...
-                    [-CellsNumber-1, CellsNumber+1], [1, 1], '-b', [1, 1],...
-                    [-CellsNumber-1, CellsNumber+1], '-b', [-1, -1],...
-                    [-CellsNumber-1, CellsNumber+1], '-b');
-    axis(fig, [-CellsNumber-1 CellsNumber+1 -CellsNumber-1 CellsNumber+1])
+                    [0, 2*CellsNumber+1], [CellsNumber-1, CellsNumber-1], '-b',...
+                    [0, 2*CellsNumber+1], [CellsNumber+1, CellsNumber+1], '-b', ...
+                    [CellsNumber+1, CellsNumber+1], [0, 2*CellsNumber+1], '-b', ...
+                    [CellsNumber-1, CellsNumber-1], [0, 2*CellsNumber+1], '-b');
+    axis(fig, [0 2*CellsNumber+1 0 2*CellsNumber+1])
     pause(0.1)
 end;
 
