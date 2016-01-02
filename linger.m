42b572e86d830d509f0aa539777a3cc468701a70
nigl <Martin-Kraus@gmx.de>
2016-01-02 17:30:00 +0100

    Richtigeres Auswahlverfahren der zu lingernden Fahrzeuge

--- linger.m
+++ linger.m
@@ -1,8 +1,15 @@
 function [ Speed ] = linger(Speed, pLinger)
 %LINGER updated den Speed geschwindigkeits vektor gemäß der übergebenen 
-% trödel wahrscheinlichkeit
-Rand = rand(size(Speed));
-Speed(Rand < pLinger) = max(Speed(Rand < pLinger)-1, 0);   
+% troedel wahrscheinlichkeit
+
+% Rand = rand(size(Speed));
+% Speed(Rand < pLinger) = max(Speed(Rand < pLinger)-1, 0); 
+
+
+nonZeroIndices = find(Speed);
+Rand = rand(size(nonZeroIndices));
+choosenIndices = nonZeroIndices(Rand<pLinger);
+Speed(choosenIndices) = max(Speed(choosenIndices)-1,0);
 
 end
 
